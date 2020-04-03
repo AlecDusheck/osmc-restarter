b2ef57ac128843ac842dc561496bd6dac8eacad9
AlecDusheck <a.dusheckirl@gmail.com>
2020-04-02 22:37:38 -0500

    Clean up

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const clear = require('clear');
 const spawn = require('child_process').spawn;
 const gamedig = require('gamedig');
 
+// Logging functions
 const logError = (str) => console.log(chalk.red(`Watchdog: (!) ${str}`));
 const logMsg = (str) => console.log(chalk.blue(`Watchdog: (*) ${str}`));
 const logChild = (str) => console.log(chalk.yellow(`Child (PID: ${child.pid}): ${str}`));
 
-clear();
+clear(); // Clear the screen
 const args = process.argv.slice(2);
 
-const port = Number.parseInt(args[0]);
-const script = args[1];
+const port = Number.parseInt(args[0]); // Get the port
+const script = args[1]; // Get the script path to run
 
 if (Number.isNaN(port) || port < 1000 || port > 75565) {
     logError('Invalid port');
@@ -21,14 +22,14 @@ if (Number.isNaN(port) || port < 1000 || port > 75565) {
     process.exit(1);
 }
 
-let child;
+let child; // The child process
 let running = false;
 let failed = 0;
 
+// Spawn the main child process
 const spawnProcess = async () => {
     // If the child is running, kill it and wait 5 seconds
     if (child) {
-        console.log(child.exitCode);
         if (child.exitCode === null) {
             logMsg('Previous unstopped child process detected (how?)... sending SIGKILL');
             child.kill('SIGKILL');
@@ -38,9 +39,12 @@ const spawnProcess = async () => {
             });
         }
 
+        // Remove previous listeners
+        child.removeAllListeners();
         child = undefined;
     }
 
+    // Create child
     child = spawn(script);
 
     child.on('error', err => {
@@ -61,41 +65,49 @@ const spawnProcess = async () => {
     child.stderr.on('data', data => logError(data));
 
     child.on('exit', async () => {
-        logError('Process ended. Rebooting server!');
+        running = false;
+        logError('Process ended unexpectedly. Rebooting server!');
         await new Promise(resolve => {
             setTimeout(() => resolve(), 5000);
         });
 
+        // Respawn the process. It ended
         spawnProcess();
     });
 };
 
+// Bootstrap the process
 spawnProcess();
 
+// Start pinging MC servers on that port
 setInterval(async () => {
-    if (!running || !child) {
+    if (!running || !child) { // Make sure the server is running
         return;
     }
 
+    // Try to ping the server
     try {
         await gamedig.query({
             type: 'minecraft',
-            host: 'oneshotmc.com',
+            host: 'localhost',
             port: port
         });
 
+        // Reset counter after a successful attempt
         logMsg('Pinged server!');
         failed = 0;
     } catch (e) {
-        if (failed >= 8) {
+        // We failed, yikes
+        if (failed >= 9) { // Restart server after 8 attempts
             logError('Force restarting server');
             failed = 0;
 
-            child.kill('SIGKILL'); // kill the process
+            child.kill('SIGKILL'); // Kill the process, we hung
         } else {
+            // Just increment the failed counter
             failed++;
             logError('Failed to ping server! (' + failed + '/8)');
         }
     }
 
-}, 20000);
+}, 2 * 1000); // Every 20 secs
